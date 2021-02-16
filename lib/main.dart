cb70b845b19c7d89c6fcafa030f1f7edb6963262
Wan Muzaffar <wanmuzaffar@Wans-MacBook-Pro.local>
2021-02-16 11:31:01 +0800

    add item

--- lib/main.dart
+++ lib/main.dart
@@ -16,41 +16,66 @@ class MyApp extends StatelessWidget {
 
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomePage(),
+      home: BMIPage(),
     );
   }
 }
-class HomePage extends StatelessWidget {
+
+class BMIPage extends StatefulWidget {
+  @override
+  _BMIPageState createState() => _BMIPageState();
+}
+
+class _BMIPageState extends State<BMIPage> {
+  var weightValue = 70.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("BMI Calculator"),backgroundColor: Colors.red),
-      body:Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Text("BMI Calculator", style: TextStyle(decoration:
-            TextDecoration.underline,
-                fontSize: 30, color: Colors.red),),
-            SizedBox(height:10),
-            Image.network("https://cdn.moreappslike.com/com.avika.bmiapplication-header.png"),
-            SizedBox(height:10),
-            Text("We care about your life"),
-            SizedBox(height:10),
-            Text("Height - 170cm"),
-            SizedBox(height:10),
-            Text("Weight - 80kg"),
-            SizedBox(height: 10,),
-            FlatButton.icon(
-              color: Colors.red,
-                textColor: Colors.white,
-                onPressed: (){ print("Hello World");},
-                icon: Icon(Icons.favorite),
-              label: Text("Calculate"),
-        ),
-        ]
+        appBar: AppBar(
+            title: Text("BMI Calculator"), backgroundColor: Colors.red),
+        body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+                children: [
+                  Text("BMI Calculator", style: TextStyle(decoration:
+                  TextDecoration.underline,
+                      fontSize: 30, color: Colors.red),),
+                  SizedBox(height: 10),
+                  Image.network(
+                      "https://cdn.moreappslike.com/com.avika.bmiapplication-header.png"),
+                  SizedBox(height: 10),
+                  Text("We care about your life"),
+                  SizedBox(height: 10),
+                  Text("Height - 170cm"),
+                  SizedBox(height: 10),
+                  Slider(
+                    value: weightValue,
+                    min: 20,
+                    max: 200,
+                    label: weightValue.round().toString(),
+                    onChanged: (double value) {
+                      setState(() {
+                        weightValue = value;
+                      });
+                    },
+                  ),
+                  SizedBox(height: 10),
+                  Text("Weight - 80kg"),
+                  SizedBox(height: 10,),
+                  FlatButton.icon(
+                    color: Colors.red,
+                    textColor: Colors.white,
+                    onPressed: () {
+                      print("Hello World");
+                    },
+                    icon: Icon(Icons.favorite),
+                    label: Text("Calculate"),
+                  ),
+                ]
+            )
         )
-      )
     );
   }
+
 }
